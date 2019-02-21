040875e724ef9f6a8846e952944bd723df861aec
Sophie van Wersch <Sophie.van.Wersch-CIC.Netherlands@ibm.com>
2019-02-21 15:08:20 +0100

    NewQuote file created with form tags

---
+++ src/assets/components/NewQuote.vue
@@ -0,0 +1,25 @@
+<template>
+import { PassThrough } from 'stream';
+    <div class="row">
+      <form>
+        <div class="col-sm-8 cold-sm-offset-2 col-xs-12 col-md-6 cold-md-offset-3">
+          <label> Quote </label>
+          <textarea class=form-control rows="3" v-model="quote"></textarea>
+        </div>
+      </form>
+    </div>
+</template>
+<script>
+  export default {
+    data: function() {
+      return {
+        quote: ''
+      };
+    }
+  }
+</script>
+<style>
+
+</style>
+
+
